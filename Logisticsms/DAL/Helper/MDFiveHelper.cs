ec1b872bf088ae2ea973afd496d6f7230c707997
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-27 22:09:48 +0800

    GetMDFive

---
+++ Logisticsms/DAL/Helper/MDFiveHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Logisticsms.DAL.Helper
+{
+    internal class MDFiveHelper
+    {
+        public static string GetMDFive(string password)
+        {
+            byte[] result = Encoding.Default.GetBytes(password);
+            MD5 mdFive = new MD5CryptoServiceProvider();
+            byte[] output = mdFive.ComputeHash(result);
+            password = BitConverter.ToString(output).Replace("-", "");
+            return password;
+        }
+    }
+}
--- Logisticsms/MainWindow.xaml.cs
+++ Logisticsms/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Logisticsms.DAL.Helper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace Logisticsms
         public MainWindow()
         {
             InitializeComponent();
+            //MDFiveHelper.GetMDFive("1");
         }
     }
 }
