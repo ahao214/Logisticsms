34d42c23c1ed9343ad414eaf0a7e4e94cff06edb
liupenghao@fulan.com.cn <haochen214@163.com>
2024-02-23 20:25:56 +0800

    显示时间和操作人员

--- Logisticsms/AppData.cs
+++ Logisticsms/AppData.cs
@@ -1,13 +1,29 @@
-﻿using Logisticsms.DAL;
+﻿using GalaSoft.MvvmLight;
+using Logisticsms.DAL;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Logisticsms
 {
     /// <summary>
     /// 全局类
     /// </summary>
-    public class AppData
+    public class AppData : ObservableObject
     {
+        public AppData()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    SystemTime = DateTime.Now;
+                }
+            });
+        }
+
+
         public static AppData Instance { get; set; } = new Lazy<AppData>().Value;
 
         /// <summary>
@@ -18,6 +34,13 @@ namespace Logisticsms
         /// 主窗体
         /// </summary>
         public MainWindow MainWindow { get; set; } = null;
+
+        private DateTime systemTime = DateTime.Now;
+        public DateTime SystemTime
+        {
+            get { return systemTime; }
+            set { systemTime = value; RaisePropertyChanged(); }
+        }
 
         public void ShowMarker()
         {
